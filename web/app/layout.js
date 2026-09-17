c0e7aec9012a54a91034540620e1275f1c19b458
Ajay_tiwari <ajaynld13@gmail.com>
2026-09-17 06:58:38 +0200

    Add web layout

---
+++ web/app/layout.js
@@ -0,0 +1,3 @@
+import './style.css';
+export const metadata={title:'Opportunity Atlas',description:'Find opportunities worth your time'};
+export default function Layout({children}){return <html><body>{children}</body></html>}
